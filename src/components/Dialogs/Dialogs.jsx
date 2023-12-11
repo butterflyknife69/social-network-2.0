3179c2f9193437e19637e04dacee5c7a02c6ab48
Igor <kishmashine@gmail.com>
2023-12-11 17:28:01 +0200

    created layout dialog window

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,40 @@
+import { NavLink } from "react-router-dom";
 import s from "./Dialogs.module.css"
 
 const Dialogs = (props) => {
     return (
-        <div className={s.dialogs}>Hello Men</div>
+        <div className={s.wraper}>
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={s.dialog + ' ' + s.active}>
+                        <NavLink to="/dialogs/1">Dimich</NavLink>
+                    </div>
+                    <div className={s.dialog}>
+                        <NavLink to="/dialogs/2">Andrew</NavLink>
+                    </div>
+                    <div className={s.dialog}>
+                        <NavLink to="/dialogs/3">Sveta</NavLink>
+                    </div>
+                    <div className={s.dialog}>
+                        <NavLink to="/dialogs/4">Sasha</NavLink>
+                    </div>
+                    <div className={s.dialog}>
+                        <NavLink to="/dialogs/5">Viktor</NavLink>
+                    </div>
+                    <div className={s.dialog}>
+                        <NavLink to="/dialogs/6">Valera</NavLink>
+                    </div>
+                </div>
+                <div className={s.messages}>
+                    <div className={s.message}>Hi</div>
+                    <div className={s.message}>How are you</div>
+                    <div className={s.message}>Hi bro</div>
+                    <div className={s.message}>What your doing</div>
+                    <div className={s.message}>Brooo</div>
+                    <div className={s.message}>Waid a minute</div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import s from "./Heder.module.css"
 const Header=(props)=>{
 return(
 <header className={s.header}>
-        <img className={s.img_header} src="https://lh3.googleusercontent.com/3bXLbllNTRoiTCBdkybd1YzqVWWDRrRwJNkVRZ3mcf7rlydWfR13qJlCSxJRO8kPe304nw1jQ_B0niDo56gPgoGx6x_ZOjtVOK6UGIr3kshpmTq46pvFObfJ2K0wzoqk36MWWSnh0y9PzgE7PVSRz6Y" alt="logo" />
+        <img className={s.img_header} src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/2048px-LEGO_logo.svg.png" alt="logo" />
       </header>
 )
 }
