fa42006bc373b11d8ed30ebd4c7bdd9135b89d18
Igor <kishmashine@gmail.com>
2023-12-21 23:06:22 +0200

    added info for  Music component

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -3,7 +3,25 @@ import s from "./Music.module.css"
 
 const Music=(props)=>{
     return(
-        <div className={s.music}>Hello</div>
+        <div className={s.music}>
+      <div className={s.pageHeader}>
+        <h2>Музика</h2>
+      </div>
+      <div className={s.musicContent}>
+        <div className={s.songList}>
+          <h3>Мої улюблені пісні</h3>
+          <ul>
+            <li>Shape of You - Ed Sheeran</li>
+            <li>Bohemian Rhapsody - Queen</li>
+            <li>Blinding Lights - The Weeknd</li>
+            {/* Додавайте більше пісень за потребою */}
+          </ul>
+        </div>
+        <div className={s.audioPlayer}>
+          <p>Ваш аудіоплеєр тут</p>
+        </div>
+      </div>
+    </div>
     )
 }
 
