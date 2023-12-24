6849269b793265de94746152a75b2d239789b8ea
Igor <kishmashine@gmail.com>
2023-12-24 22:39:49 +0200

    added Settings marking

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -2,9 +2,20 @@
 import s from "./News.module.css"
 
 
-const News=(props)=>{
-    return(
-        <div className={s.news}>Hello</div>
+const News = (props) => {
+    return (
+        <div className={s.news}>
+            <h2>Заголовок новин</h2>
+            <img src="шлях_до_зображення.jpg" alt="Опис зображення" />
+            <p>
+                Тут може бути текст вашої новини. Також можна додавати інші елементи, такі як зображення.
+            </p>
+            <div className={s['news-info']}>
+                <span>Дата: 24 грудня 2023</span>
+                <span>Автор: Ім'я Автора</span>
+            </div>
+            
+        </div>
     )
 }
 
--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,11 +1,60 @@
+// Settings.js
 
-import s from "./Settings.module.css"
+import React from "react";
+import s from "./Settings.module.css";
 
-
-const Settings=(props)=>{
-    return(
-        <div className={s.settings}>Hello</div>
-    )
-}
+const Settings = (props) => {
+    return (
+        <div className={s.settings}>
+            <h2 className={s.title}>Налаштування</h2>
+            <div className={s.settingsContent}>
+                <div className={s.settingSection}>
+                    <h3 className={s.sectionTitle}>Загальні налаштування</h3>
+                    <div className={s.settingItem}>
+                        <label>Мова сайту:</label>
+                        <select>
+                            <option>Українська</option>
+                            <option>English</option>
+                            <option>Русский</option>
+                        </select>
+                    </div>
+                    <div className={s.settingItem}>
+                        <label>Тема оформлення:</label>
+                        <select>
+                            <option>Світла</option>
+                            <option>Темна</option>
+                        </select>
+                    </div>
+                    {/* Додайте інші налаштування загального розділу */}
+                </div>
+                <div className={s.settingSection}>
+                    <h3 className={s.sectionTitle}>Профіль</h3>
+                    <div className={s.settingItem}>
+                        <label>Ім'я:</label>
+                        <input type="text" />
+                    </div>
+                    <div className={s.settingItem}>
+                        <label>Email:</label>
+                        <input type="email" />
+                    </div>
+                    {/* Додайте інші налаштування для профілю */}
+                </div>
+                <div className={s.settingSection}>
+                    <h3 className={s.sectionTitle}>Безпека</h3>
+                    <div className={s.settingItem}>
+                        <label>Змінити пароль:</label>
+                        <input type="password" />
+                    </div>
+                    <div className={s.settingItem}>
+                        <label>Двоетапна аутентифікація:</label>
+                        <input type="checkbox" />
+                    </div>
+                    {/* Додайте інші налаштування для безпеки */}
+                </div>
+                {/* Додайте інші розділи та налаштування за необхідності */}
+            </div>
+        </div>
+    );
+};
 
 export default Settings;
