8d02551e79f11f933ff363c520292b1d1129bd41
Igor <kishmashine@gmail.com>
2023-12-28 06:16:22 +0200

    create a visualization on the site

--- src/App.jsx
+++ src/App.jsx
@@ -18,18 +18,19 @@ const App = (props) => {
 
     <div className="app-wraper">
       <HeaderContainer />
-      <Navbar />
-      <div className='appWrapperContent'>
-        <Routes>
-
-          <Route path="/profile/:userId?" element={<ProfileContainer />} />
-          <Route path="/dialogs/*" element={<DialogsContainer />} />
-          <Route path="/users" element={<UsersContainer/>} />
-          <Route path="/news" element={<News />} />
-          <Route path="/music" element={<Music />} />
-          <Route path="/video" element={<Video />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes>
+      <div className='appWrapperContentNavbar'>
+        <Navbar />
+        <div className='appWrapperContent'>
+          <Routes>
+            <Route path="/profile/:userId?" element={<ProfileContainer />} />
+            <Route path="/dialogs/*" element={<DialogsContainer />} />
+            <Route path="/users" element={<UsersContainer />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/video" element={<Video />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
       </div>
     </div>
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,15 +4,22 @@ import s from "./Heder.module.css"
 const Header = (props) => {
   return (
     <header className={s.header}>
-      <img className={s.img_header} src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/2048px-LEGO_logo.svg.png" alt="logo" />
-      <div className={s.loginBlock}>
+      <div className={s.wraperItemHeader}>
+        <div  className={s.imgWraper}>
+          <img className={s.img_header} src="https://assets.stickpng.com/images/62e131df7fe3599fdd46ecb3.png" alt="logo" />
+        </div>
+        <div className={s.swichDark}>
+          <button id="switchMode">Switch</button>
+        </div>
+        <div className={s.loginBlock}>
         {props.isAuth ? props.login
           : <NavLink to='/login'>Login</NavLink>
         }
-
+      </div>
       </div>
     </header>
   )
 }
 
 export default Header;
+
